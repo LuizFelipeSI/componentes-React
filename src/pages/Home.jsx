d86441747e2cdea9c42ba90b4479a6346b884f7d
Luiz Felipe <lfpereira323@gmail.com>
2024-06-11 23:53:23 -0300

    mudança do metodo de ativar usuário

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,9 @@ function Home() {
   const [nome, setNome] = useState("")
 
   useEffect(() => {
-    if(nome != "" && nome.length > 3 && !isLoading){
+    if (nome != "" && nome.length > 3 && !isLoading) {
       setUsuarios(usuarios.filter(usuario => usuario.nome.toLowerCase().includes(nome.toLowerCase())))
-    }else if(isLoading || nome.length == 0){
+    } else if (isLoading || nome.length == 0) {
       axios.get(`http://localhost:3000/usuarios`)
         .then(res => {
           setUsuarios(res.data)
@@ -30,18 +30,18 @@ function Home() {
     }
   }, [isLoading, nome]) //Deixe o array vazio caso queira que o código seja executado apenas uma vez durante o ciclo de vida do componente
 
-  function mascararEmail(email){
+  function mascararEmail(email) {
     let emailMascarado = email[0]
     let mostrarCaracter = false
 
-    for(let i = 1; i < email.length; i++){
-      if(email[i] == '@'){
+    for (let i = 1; i < email.length; i++) {
+      if (email[i] == '@') {
         mostrarCaracter = true
       }
 
-      if(!mostrarCaracter){
+      if (!mostrarCaracter) {
         emailMascarado += '*'
-      }else{
+      } else {
         emailMascarado += email[i]
       }
     }
@@ -50,7 +50,7 @@ function Home() {
     return emailMascarado
   }
 
-  function inativarUsuario(id){
+  function inativarUsuario(id) {
     // PATCH
     axios.patch(`http://localhost:3000/usuarios/${id}`, { ativo: 0 })
       .then(res => {
@@ -63,12 +63,12 @@ function Home() {
       })
   }
 
-  function ativarUsuario(id){
+  function ativarUsuario(id) {
     // PATCH
     axios.patch(`http://localhost:3000/usuarios/${id}`, { ativo: 1 })
       .then(res => {
         console.log(res.data)
-        setUsuarios([]) // Força a atualização da lista de usuários
+        setIsLoading(true) // Força a atualização da lista de usuários
       })
       .catch(res => {
         console.log(res.data)
@@ -76,89 +76,85 @@ function Home() {
       })
   }
 
-  function removerUsuario(id){
-    
+  function removerUsuario(id) {
     axios.delete(`http://localhost:3000/usuarios/${id}`)
-      
-        
-        setIsLoading(true) // Força a atualização da lista de usuários
-        setUsuarios([]) // Força a atualização da lista de usuários
-      
-      
+    
+    setIsLoading(true) // Força a atualização da lista de usuários
+    setUsuarios([]) // Força a atualização da lista de usuários
   }
 
-  function atualizaNome(event){
+  function atualizaNome(event) {
     setNome(event.target.value)
   }
 
-  if(isLoading){
+  if (isLoading) {
     // Mostra a mensagem de carregando enquanto atualiza a página
     return (
       <h1>Carregando</h1>
     )
-  }else{
+  } else {
     return (
-      <> 
+      <>
         <div className='row'>
-            <div className='col-3'>
-                <label htmlFor="_nome">Nome</label>
-                <input type="text" id="_nome" placeholder="Digite para pesquisar" className='form-control' onChange={atualizaNome} value={nome} />
+          <div className='col-3'>
+            <label htmlFor="_nome">Nome</label>
+            <input type="text" id="_nome" placeholder="Digite para pesquisar" className='form-control' onChange={atualizaNome} value={nome} />
+          </div>
+          <div className='col'>
+            <div className='d-flex align-items-end justify-content-end'>
+              <Link to={"/cadastro"} className="btn btn-primary">Novo registro</Link>
             </div>
-            <div className='col'>
-                <div className='d-flex align-items-end justify-content-end'>
-                    <Link to={"/cadastro"} className="btn btn-primary">Novo registro</Link>
-                </div>
-            </div>
+          </div>
         </div>
         <div className='row'>
-            <div className='col'>
-                <Titulo>Usuários Cadastrados</Titulo>
-                { erro && <div className='alert alert-danger'>{erro}</div> }
-                <table className='table table-striped'>
-                <thead>
-                    <tr>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Cargo</th>
-                    <th>Usuário GH</th>
-                    <th>E-mail</th>
-                    <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                    usuarios.map(usuario => {
-                        return (
-                        <tr key={usuario.id}>
-                            <td>{usuario.id}</td>
-                            <td>{usuario.nome}</td>
-                            <td>{usuario.cargo}</td>
-                            <td>{usuario.login}</td>
-                            <td>{mascararEmail(usuario.email)}</td>
-                            <td>
-                            {
-                                usuario.ativo == 1 && <Botao tipo="danger" acao={() => inativarUsuario(usuario.id)}>Desativar</Botao>
-                            }
-                            {
-                                usuario.ativo == 0 && <Botao tipo="success" acao={() => ativarUsuario(usuario.id)}>Ativar</Botao>
-                            }
-                            {
-                                <Botao tipo="danger" acao={() => removerUsuario(usuario.id)}>Excluir</Botao>
-                            }
-                            </td>
-                        </tr>
-                        )
-                    })
-                    }
-                </tbody>
-                </table>
-            </div>
+          <div className='col'>
+            <Titulo>Usuários Cadastrados</Titulo>
+            {erro && <div className='alert alert-danger'>{erro}</div>}
+            <table className='table table-striped'>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Nome</th>
+                  <th>Cargo</th>
+                  <th>Usuário GH</th>
+                  <th>E-mail</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  usuarios.map(usuario => {
+                    return (
+                      <tr key={usuario.id}>
+                        <td>{usuario.id}</td>
+                        <td>{usuario.nome}</td>
+                        <td>{usuario.cargo}</td>
+                        <td>{usuario.login}</td>
+                        <td>{mascararEmail(usuario.email)}</td>
+                        <td>
+                          {
+                            usuario.ativo == 1 && <Botao tipo="danger" acao={() => inativarUsuario(usuario.id)}>Desativar</Botao>
+                          }
+                          {
+                            usuario.ativo == 0 && <Botao tipo="success" acao={() => ativarUsuario(usuario.id)}>Ativar</Botao>
+                          }
+                          {
+                            <Botao tipo="danger" acao={() => removerUsuario(usuario.id)}>Excluir</Botao>
+                          }
+                        </td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
       </>
     )
   }
-  
-  
+
+
 }
 
 export default Home
